73763d59ed08e9e19d9f5e1d7527e68ef7c234dc
Noah Osterhout <noahosterhout@Noahs-MacBook-Pro.local>
2020-04-13 14:36:40 -0400

    Added basic routing to pages/Index.js

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e53c960699d71bc5f49f";
+/******/ 	var hotCurrentHash = "54794d7cf7ab57c54709";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
